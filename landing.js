d22a1bb3bc2515c63b062f62832bd68bbb3c84be
Shaunbag <shaunbag@hotmail.com>
2021-06-17 09:27:25 +0100

    Further styling to landing page

--- landing.js
+++ landing.js
@@ -7,77 +7,6 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext('2d'); 
 
 
-
-/*
-
-//Rectangle
-context.fillStyle =' rgba(30, 255, 0, 0.7)'; // set color of rectangle
-context.fillRect(150, 299, 300, 500); // creat a rectangle (color filled) on the (x axis, y axis, width and height) within the canvas
-
-/*
-//create a line on the canvas
-context.beginPath();  // create a new element which is unconnected from the previous rectangle created 
-context.moveTo(60, 500); //starting coordinate of the line
-context.lineTo(600, 150); // moving to this coordinate
-context.lineTo(800, 900);
-context.strokeStyle = 'rgba(255, 40, 0, 0.5)'; // set color of line
-context.stroke();
-
-// Arcs and circles
-context.beginPath(); // begin new element disconnected from the above Lines
-context.arc(900, 300, 60, 0, Math.PI * 2, false);
-context.strokeStyle = 'green';
-context.stroke();
-
-
-// For loop to create mulciple elements to the canvas, in this case circles
-
-for(let i = 0; i < 5; i++){
-
-    let y = Math.random() * window.innerHeight;  //set the y axis to a randomly generated numer saved as the y variable
-    let x = Math.random() * window.innerWidth; // set the x axis the same, * by the window innerheight and width so that it randomly places coordinates within the canvas
-    let rad = Math.random() * 100; // random radius for the circle
-    let a = Math.random() * 255;
-    let b = Math.random() * 255;// set rgb values to random numbers between 0 and 255
-    let c = Math.random() * 255;
-    context.beginPath();
-    context.arc(x, y, rad, 0, Math.PI * 2, false); // arc function created using the variables above
-    context.strokeStyle = `rgb(${a}, ${b}, ${c})`; // color styling using the random variables created above
-    context.stroke();
-}
-
-// for loop for multiple lines
-
-for(let i = 0; i < 20; i++){
-
-    let x1 = Math.random() * window.innerWidth;
-    let y1 = Math.random() * window.innerHeight;
-    let x2 = Math.random() * window.innerWidth;
-    let y2 = Math.random() * window.innerHeight;
-    let a = Math.random() * 255;
-    let b = Math.random() * 255;// set rgb values to random numbers between 0 and 255
-    let c = Math.random() * 255;
-    context.beginPath();
-    context.moveTo(x1, y2);
-    context.lineTo(x2, y1);
-    context.strokeStyle = `rgb(${a}, ${b}, ${c})`;
-    context.stroke();
-}
-
-for(let i = 0; i < 10; i++){
-    let x = Math.random() * innerWidth;
-    let y = Math.random() * innerHeight;
-    let w = Math.random() * 300;
-    let h = Math.random() * 500;
-    let a = Math.random() * 255;
-    let b = Math.random() * 255;// set rgb values to random numbers between 0 and 255
-    let c = Math.random() * 255;
-    context.beginPath();
-    context.fillRect(x, y, w, h);
-    context.fillStyle = `rgb(${a}, ${b}, ${c})`;
-} */
-
-
 //Animate circle
 
 function Circle(x, y, dx, dy, radius){  // create a circle object with own methods
@@ -129,7 +58,7 @@ for(let i = 0; i < 17; i++){
 function animate() { // the function 'Animate'
     requestAnimationFrame(animate); // this function calls the animate function over and over and over
     context.clearRect(0, 0, innerWidth, innerHeight); // clear the canvas so that the circle appears to move rather than drawing a solid line of cicles
-        
+      
     for (i = 0; i < circleArr.length; i++){
         circleArr[i].update();// callig the object function update on each circle in the array
     }
